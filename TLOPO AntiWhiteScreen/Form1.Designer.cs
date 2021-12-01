6aae403222e15940e8f78f70b6e76c4dcb8ab8cc
37062 <37062@LAPTOP-JDGENKSD>
2021-12-01 23:44:47 +0200

    Add project files.

---
+++ TLOPO AntiWhiteScreen/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace TLOPO_AntiWhiteScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RamAvailability = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GameRunning = new System.Windows.Forms.Label();
+            this.CacheCleared = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.RamAvailable = new System.Windows.Forms.Label();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.Width = 0;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Rounding = 12;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(370, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Developed by Raven#0404";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(103, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TLOPO Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(104, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Running:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(7, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 28);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "RAM Availability:";
+            // 
+            // RamAvailability
+            // 
+            this.RamAvailability.Location = new System.Drawing.Point(12, 136);
+            this.RamAvailability.Name = "RamAvailability";
+            this.RamAvailability.Size = new System.Drawing.Size(352, 39);
+            this.RamAvailability.Step = 1;
+            this.RamAvailability.TabIndex = 6;
+            this.RamAvailability.Value = 50;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(324, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 28);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "System Info:";
+            // 
+            // GameRunning
+            // 
+            this.GameRunning.AutoSize = true;
+            this.GameRunning.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameRunning.Location = new System.Drawing.Point(182, 51);
+            this.GameRunning.Name = "GameRunning";
+            this.GameRunning.Size = new System.Drawing.Size(37, 17);
+            this.GameRunning.TabIndex = 8;
+            this.GameRunning.Text = "False";
+            // 
+            // CacheCleared
+            // 
+            this.CacheCleared.AutoSize = true;
+            this.CacheCleared.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CacheCleared.Location = new System.Drawing.Point(182, 76);
+            this.CacheCleared.Name = "CacheCleared";
+            this.CacheCleared.Size = new System.Drawing.Size(148, 17);
+            this.CacheCleared.TabIndex = 9;
+            this.CacheCleared.Text = "TLOPO folder not found";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(104, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Cache: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(325, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "RAM:";
+            // 
+            // RamAvailable
+            // 
+            this.RamAvailable.AutoSize = true;
+            this.RamAvailable.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RamAvailable.Location = new System.Drawing.Point(370, 49);
+            this.RamAvailable.Name = "RamAvailable";
+            this.RamAvailable.Size = new System.Drawing.Size(133, 17);
+            this.RamAvailable.TabIndex = 12;
+            this.RamAvailable.Text = "Not enough available";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(329, 74);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(124, 28);
+            this.kryptonButton1.TabIndex = 13;
+            this.kryptonButton1.Values.Text = "Set TLOPO directory";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(522, 180);
+            this.Controls.Add(this.kryptonButton1);
+            this.Controls.Add(this.RamAvailable);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.CacheCleared);
+            this.Controls.Add(this.GameRunning);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.RamAvailability);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TLOPO AntiWhiteScreen";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar RamAvailability;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label GameRunning;
+        private System.Windows.Forms.Label CacheCleared;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label RamAvailable;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+    }
+}
+
---
+++ TLOPO AntiWhiteScreen/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace TLOPO_AntiWhiteScreen
+{
+    public partial class Form1 : KryptonForm
+    {
+        public string TargetDir = "C:/Program Files/TLOPO";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            //the main brain
+            while (true)
+            {
+                //check if tlopo is running
+                if (Process.GetProcessesByName("tlopo").Length > 0)
+                {
+                    // is running
+                    GameRunning.Text = "True";
+                    GameRunning.ForeColor = Color.Green;
+                }
+                else
+                {
+                    // isnt running
+                    GameRunning.Text = "False";
+                    GameRunning.ForeColor = Color.Red;
+                }
+                //get total ram, and available ram
+                Int64 phav = PerformanceInfo.GetPhysicalAvailableMemoryInMiB();
+                Int64 tot = PerformanceInfo.GetTotalMemoryInMiB();
+                //turn it into percents
+                decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+                decimal percentOccupied = 100 - percentFree;
+                //update progressbar
+                RamAvailability.Value = (int)percentOccupied;
+                //check if available ram is less than 1gb
+                if (phav < 1000)
+                {
+                    //not enough ram
+                    RamAvailable.Text = "Not enough available";
+                    RamAvailable.ForeColor = Color.Red;
+                } else
+                {
+                    //enough ram
+                    RamAvailable.Text = "Enough RAM available";
+                    RamAvailable.ForeColor = Color.Green;
+                }
+                
+                //check if tlopo directory exists
+                if (Directory.Exists(TargetDir))
+                {
+                    //it exists, check for cache-test folder
+                    if (Directory.Exists(TargetDir+"/cache-test"))
+                    {
+                        //it exists, time to clean up
+                        CacheCleared.Text = "Cache NOT cleared";
+                        CacheCleared.ForeColor = Color.Red;
+                        //delay to prevent constantly deleting subfiles while loading maps
+                        await Task.Delay(2000);
+                        //get all subfiles
+                        string[] files = Directory.GetFiles(TargetDir+"/cache-test");
+                        //delete subfiles
+                        foreach (string file in files)
+                        {
+                            File.SetAttributes(file, FileAttributes.Normal);
+                            File.Delete(file);
+                        }
+                        //try to delete main folder, placed in try catch to prevent errors incase subfiles are created while deleting main folder
+                        try
+                        {
+                            Directory.Delete(TargetDir+"/cache-test", false);
+                        }
+                        catch (IOException v)
+                        {
+                            //throw away error, next loop will attempt again, cant be assed making this recursively delete subfiles until its confirmed to all be gone
+                            if (v.Source != null)
+                                Console.WriteLine("IOException source: {0}", v.Source);
+                            throw;
+                        }
+                        
+                    } else
+                    {
+                        //no cache-test folder found, assume cleared
+                        CacheCleared.Text = "Cache cleared";
+                        CacheCleared.ForeColor = Color.Green;
+                    }
+                } else
+                {
+                    //tlopo directory not found
+                    CacheCleared.Text = "TLOPO folder not found";
+                    CacheCleared.ForeColor = Color.Red;
+                }
+                //pause the loop to stop cpu from having a stroke, and allow GUI to update
+                await Task.Delay(5000);
+            }
+        }
+
+        //ngl this is some shit i took off stackoverflow, reading ram is a pain in the ass
+        public static class PerformanceInfo
+        {
+            [DllImport("psapi.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct PerformanceInformation
+            {
+                public int Size;
+                public IntPtr CommitTotal;
+                public IntPtr CommitLimit;
+                public IntPtr CommitPeak;
+                public IntPtr PhysicalTotal;
+                public IntPtr PhysicalAvailable;
+                public IntPtr SystemCache;
+                public IntPtr KernelTotal;
+                public IntPtr KernelPaged;
+                public IntPtr KernelNonPaged;
+                public IntPtr PageSize;
+                public int HandlesCount;
+                public int ProcessCount;
+                public int ThreadCount;
+            }
+
+            public static Int64 GetPhysicalAvailableMemoryInMiB()
+            {
+                PerformanceInformation pi = new PerformanceInformation();
+                if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+                {
+                    return Convert.ToInt64((pi.PhysicalAvailable.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+                }
+                else
+                {
+                    return -1;
+                }
+
+            }
+
+            public static Int64 GetTotalMemoryInMiB()
+            {
+                PerformanceInformation pi = new PerformanceInformation();
+                if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+                {
+                    return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+                }
+                else
+                {
+                    return -1;
+                }
+
+            }
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            //user wants to change default location, warn them first
+            DialogResult dialogResult = MessageBox.Show("Are you sure you know what you are doing?\n\nPerfoming this action may delete files that you do not wish to delete.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //user REALLY wants to change default location, ok, open folder explorer
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.ShowNewFolderButton = false;
+                fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+                //check if user confirmed TLOPO location
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    //user confirmed, update target directory with new directory value
+                    TargetDir = fbd.SelectedPath;
+                }
+            }
+        }
+    }
+}
